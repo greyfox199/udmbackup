8428226c5fff3e00258a1d3fe55ac4f3e54e9972
Joseph Banda <jbanda@gmail.com>
2022-05-06 17:39:48 -0500

    updated email report logic

--- udmprobackup.ps1
+++ udmprobackup.ps1
@@ -3,13 +3,15 @@ param (
   [Parameter (Mandatory = $true)] [String]$ConfigFilePath
 )
 
-$startTime = Get-Date
-
 if (-not(Test-Path -Path $ConfigFilePath -PathType Leaf)) {
   throw "json config file specified at $($ConfigFilePath) does not exist, aborting process"
 }
 
-$PowerShellObject=Get-Content -Path $ConfigFilePath | ConvertFrom-Json
+try {
+  $PowerShellObject=Get-Content -Path $ConfigFilePath | ConvertFrom-Json
+} catch {
+  throw "Config file of $($ConfigFilePath) is not a valid json file, aborting process"
+}
 
 if (-not(Test-Path -Path $PowerShellObject.Required.logsDirectory -PathType Container)) {
   throw "log root directory of $($PowerShellObject.Required.logsDirectory) does not exist, aborting process"
@@ -23,12 +25,26 @@ if (-not(Test-Path -Path $PowerShellObject.Required.localBackupDirectory -PathTy
   throw "local backup path of $($PowerShellObject.Required.localBackupDirectory) does not exist, aborting process"
 }
 
+[string] $strTimeStamp = $(get-date -f yyyy-MM-dd-hh_mm_ss)
+[string] $strDetailLogFilePath = $PowerShellObject.Required.logsDirectory + "\udmpro-backup-detail-" + $strTimeStamp + ".log"
+$objDetailLogFile = [System.IO.StreamWriter] $strDetailLogFilePath
+
+#********************************************************************************
+#functions
+#********************************************************************************
+#function to write log file
+Function LogWrite($objLogFile, [string]$strLogstring, [bool]$DisplayInConsole=$true)
+{ 
+  if ($DisplayInConsole -eq $true) {
+    write-host $strLogstring
+  }
+  $objLogFile.writeline($strLogstring)
+  $objLogFile.flush()
+}
 
 #set up variables
 [string] $strExecDir = $PSScriptRoot
 [string] $strServerName = $env:computername
-[string] $strDetailLogFilePrefix = "udmpro-backup-detail-"
-[string] $strLogExtension = ".log"
 
 [uint16] $intDaysToKeepUDMBackups = 0
 [uint16] $intDaysToKeepLogFiles = 0
@@ -39,24 +55,33 @@ if (-not(Test-Path -Path $PowerShellObject.Required.localBackupDirectory -PathTy
 
 [bool] $blnSendSMTPErrorReport = $false
 [bool] $blnSMTPAuthRequired = $false
+[bool] $blnUseSSLSMTP = $false
+[uint16] $intSMTPPort = 587
 
 if ($PowerShellObject.Optional.daysToKeepUDMBackups) {
-  write-host "am I here"
   try {
     $intDaysToKeepUDMBackups = $PowerShellObject.Optional.daysToKeepUDMBackups
-    write-host "using what is configured in config for udm backups to keep"
+    LogWrite $objDetailLogFile "$(get-date) Info: Using $($PowerShellObject.Optional.daysToKeepUDMBackups) value specified in config file for backup retention"
   } catch {
-    write-host "defaulting to no backup purge"
+    LogWrite $objDetailLogFile "$(get-date) Warning: $($PowerShellObject.Optional.daysToKeepUDMBackups) value specified in config file is not valid, defaulting to unlimited backup retention"
   }
 }
 
 if ($PowerShellObject.Optional.daysToKeepLogFiles) {
-  write-host "am I here"
   try {
     $intDaysToKeepLogFiles = $PowerShellObject.Optional.daysToKeepLogFiles
-    write-host "using what is configured in config for log file retention"
+    LogWrite $objDetailLogFile "$(get-date) Info: Using $($PowerShellObject.Optional.daysToKeepLogFiles) value specified in config file for log retention"
   } catch {
-    write-host "defaulting to no log file purge"
+    LogWrite $objDetailLogFile "$(get-date) Warning: $($PowerShellObject.Optional.daysToKeepLogFiles) value specified in config file is not valid, defaulting to unlimited log retention"
+  }
+}
+
+if ($PowerShellObject.Optional.smtpport) {
+  try {
+    $intSMTPPort = $PowerShellObject.Optional.smtpport
+    LogWrite $objDetailLogFile "$(get-date) Info: Using $($PowerShellObject.Optional.smtpport) value specified in config file for log for smtp port"
+  } catch {
+    LogWrite $objDetailLogFile "$(get-date) Warning: $($PowerShellObject.Optional.smtpport) value specified in config file is not valid, defaulting to $($intSMTPPort)"
   }
 }
 
@@ -64,6 +89,7 @@ if ($PowerShellObject.Optional.sendEmailError -and $PowerShellObject.Optional.se
   if ($PowerShellObject.Optional.smtpServer -and $PowerShellObject.Optional.emailReportRecipient) {
     $blnSendSMTPErrorReport = $true
     write-host "we're sending smtp messages"
+    LogWrite $objDetailLogFile "$(get-date) Info: Sending email error report via $($PowerShellObject.Optional.smtpServer) to $($PowerShellObject.Optional.emailReportRecipient) as specified in config file"
     if ($PowerShellObject.Optional.smtpauthrequired) {
       if ($PowerShellObject.Optional.smtpauthrequired.toLower().equals("true")) {
         $blnSMTPAuthRequired = $true
@@ -91,29 +117,15 @@ if ($PowerShellObject.Optional.udmRemoteBackupDirectory) {
   $strUDMRemoteBackupDirectory = $PowerShellObject.Optional.udmRemoteBackupDirectory
 }
 
-[string] $strTimeStamp = $(get-date -f yyyy-MM-dd-hh_mm_ss)
+
 
 [int] $intErrorCount = 0
 $arrStrErrors = @()
 
-[string] $strDetailLogFilePath = $PowerShellObject.Required.logsDirectory + "\" + $strDetailLogFilePrefix + $strTimeStamp + $strLogExtension
+
 
 #clear all errors before starting
 $error.Clear()
-
-$objDetailLogFile = [System.IO.StreamWriter] $strDetailLogFilePath
-
-#********************************************************************************
-#functions
-#********************************************************************************
-#function to write log file
-Function LogWrite($objLogFile, [string]$strLogstring)
-{  
-  write-host $strLogstring
-  $objLogFile.writeline($strLogstring)
-  $objLogFile.flush()
-}
-
 
 
 LogWrite $objDetailLogFile "$(get-date) Info: Beginning process to backup UDM Pro via scp at $($strUDMIPHostname) with $($strUDMUsername), copying $($strUDMRemoteBackupDirectory) to $($PowerShellObject.Required.localBackupDirectory)"
@@ -124,7 +136,7 @@ try {
   $objPassword = Get-Content $PowerShellObject.Required.udmPasswordFile | ConvertTo-SecureString
 	$objCredential = New-Object System.Management.Automation.PSCredential ($strUDMUsername, $objPassword)
 	LogWrite $objDetailLogFile "$(get-date) Info: Backing up UDM Pro..."
-	Get-SCPItem -AcceptKey -ComputerName $strUDMIPHostname -Credential $objCredential -Path $strUDMRemoteBackupDirectory -PathType Directory -Destination $PowerShellObject.Required.localBackupDirectory
+	Get-SCPItem -AcceptKey -ComputerName $strUDMIPHostname -Credential $objCredential -Path $strUDMRemoteBackupDirectory -PathType Directory -Destination $PowerShellObject.Required.localBackupDirectory -ErrorAction Stop
 	LogWrite $objDetailLogFile "$(get-date) Info: Successfully backed up UDM Pro"
 } catch {
 	$ErrorMessage = $_.Exception.Message
@@ -133,27 +145,58 @@ try {
 	LogWrite $objDetailLogFile "$(get-date) Error: Failed to connect to UDM Pro at $($strUDMIPHostname) with $($strUDMUsername), copying $($strUDMRemoteBackupDirectory) to $($PowerShellObject.Required.localBackupDirectory) at $($line) with the following error: $ErrorMessage"
 }
 
-if ($blnSendSMTPErrorReport -eq $true) {
-  [int] $intErrorCount = $arrStrErrors.Count
-  if ($intErrorCount -gt 0) {
+
+[int] $intErrorCount = $arrStrErrors.Count
+
+#sending email report
+if ($PowerShellObject.Optional.sendEmailError -and $PowerShellObject.Optional.sendEmailError.toLower().equals("true") -and $intErrorCount -gt 0) {
+  #if smtp server, sender and email recipient are populated
+  if ($PowerShellObject.Optional.smtpServer -and $PowerShellObject.Optional.smtpUsername -and $PowerShellObject.Optional.emailReportRecipient) {
+    $blnSendSMTPErrorReport = $true
+    write-host "here"
+    
     LogWrite $objDetailLogFile "$(get-date) Info: Encountered $intErrorCount errors, sending error report email"
   
     #loop through all errors and add them to email body
     foreach ($strErrorElement in $arrStrErrors) {
-    $intErrorCounter = $intErrorCounter + 1
-    $strEmailBody = $strEmailBody + $intErrorCounter.toString() + ") " + $strErrorElement + "`n"
+      $intErrorCounter = $intErrorCounter + 1
+      $strEmailBody = $strEmailBody + $intErrorCounter.toString() + ") " + $strErrorElement + "`n"
     }
     $strEmailBody = $strEmailBody + "`n`nPlease see $strDetailLogFilePath on $strServerName for more details"
-    if ($blnSMTPAuthRequired -eq $true) {
-      #$objSMTPPassword = ConvertTo-SecureString $strSMTPPassword -AsPlainText -Force
+
+    LogWrite $objDetailLogFile "$(get-date) Info: Sending email error report via $($PowerShellObject.Optional.smtpServer) from $($PowerShellObject.Optional.smtpUsername) to $($PowerShellObject.Optional.emailReportRecipient) as specified in config file"
+    #if smtp auth is required, handle credentials
+    if ($PowerShellObject.Optional.smtpauthrequired -and $PowerShellObject.Optional.smtpauthrequired.toLower().equals("true")) {
+      $blnSMTPAuthRequired = $true
       $objPassword = Get-Content $PowerShellObject.Optional.smtpPasswordFile | ConvertTo-SecureString
       $objSMTPCredential = New-Object System.Management.Automation.PSCredential ($PowerShellObject.Optional.smtpUsername, $objPassword)
-      send-MailMessage -From $PowerShellObject.Optional.smtpUsername -To $PowerShellObject.Optional.emailReportRecipient -SmtpServer $PowerShellObject.Optional.smtpServer -Subject "<<UDM Pro Backup>> Errors during process" -Body $strEmailBody  -UseSsl -Port 587 -Credential $objSMTPCredential
+      if ($PowerShellObject.Optional.sendEmailError -AND $PowerShellObject.Optional.sendEmailError -AND (Test-Path -Path $PowerShellObject.Optional.smtpPasswordFile -PathType Leaf)) {
+        #if smtp ssl is required, send mail with ssl flag
+        if ($PowerShellObject.Optional.smtpsslrequired.toLower().equals("true")) {
+          send-MailMessage -From $PowerShellObject.Optional.smtpUsername -To $PowerShellObject.Optional.emailReportRecipient -SmtpServer $PowerShellObject.Optional.smtpServer -Subject "<<UDM Pro Backup>> Errors during process" -Body $strEmailBody  -UseSsl -Port $intSMTPPort -Credential $objSMTPCredential
+        #else smtp ssl is NOT required, so so not use ssl flag when sending mail
+        } else {
+          send-MailMessage -From $PowerShellObject.Optional.smtpUsername -To $PowerShellObject.Optional.emailReportRecipient -SmtpServer $PowerShellObject.Optional.smtpServer -Subject "<<UDM Pro Backup>> Errors during process" -Body $strEmailBody  -Port $intSMTPPort -Credential $objSMTPCredential
+        }
+      } else {
+        LogWrite $objDetailLogFile "$(get-date) Warning: SMTP Auth set to required in config file but username or password file aren't set, aborting smtp send"
+      }
+    #else smtp auth is NOT required, so continue without credentials
     } else {
-      send-MailMessage -From $PowerShellObject.Optional.smtpUsername -To $PowerShellObject.Optional.emailReportRecipient -SmtpServer $PowerShellObject.Optional.smtpServer -Subject "<<UDM Pro Backup>> Errors during process" -Body $strEmailBody  -UseSsl -Port 587
+      LogWrite $objDetailLogFile "$(get-date) Info: Sending email report via unauthenticated session"
+      if ($PowerShellObject.Optional.smtpsslrequired.toLower().equals("true")) {
+        send-MailMessage -From $PowerShellObject.Optional.smtpUsername -To $PowerShellObject.Optional.emailReportRecipient -SmtpServer $PowerShellObject.Optional.smtpServer -Subject "<<UDM Pro Backup>> Errors during process" -Body $strEmailBody  -UseSsl -Port $intSMTPPort
+      #else smtp ssl is NOT required, so so not use ssl flag when sending mail
+      } else {
+        send-MailMessage -From $PowerShellObject.Optional.smtpUsername -To $PowerShellObject.Optional.emailReportRecipient -SmtpServer $PowerShellObject.Optional.smtpServer -Subject "<<UDM Pro Backup>> Errors during process" -Body $strEmailBody  -Port $intSMTPPort
+      }
     }
+  #smtp server, sender and/or recipient are NOT populated
+  } else {
+    LogWrite $objDetailLogFile "$(get-date) Warning: Send email error report set to true in config, but smtp server, sender and/or recipient left blank in config file, aborting sending of email error report"
   }
 }
+
 LogWrite $objDetailLogFile "$(get-date) Info: Process Complete"
 
 $objDetailLogFile.close()
